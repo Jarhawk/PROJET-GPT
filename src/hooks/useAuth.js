2fd194cbf67cde4d05ed1dcbd349c65454fed840
Jarhawk <dark_territory@msn.com>
2025-07-13 21:22:44 +0200

    feat: align user roles with role_id

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -11,6 +11,7 @@ export default function useAuth() {
     mama_id: ctx.userData?.mama_id ?? ctx.mama_id,
     access_rights: ctx.userData?.access_rights ?? ctx.access_rights,
     role: ctx.userData?.role ?? ctx.role,
+    role_id: ctx.userData?.role_id ?? ctx.role_id,
     email: ctx.userData?.email ?? ctx.email,
     actif: ctx.userData?.actif ?? ctx.actif,
     loading,

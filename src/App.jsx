90dce94012746a8d4a9247582e7c60db76f6598e
Jarhawk <dark_territory@msn.com>
2025-07-22 10:14:40 +0200

    feat: improve routing with preload and caching

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,32 @@
 // MamaStock © 2025 - Licence commerciale obligatoire - Toute reproduction interdite sans autorisation.
 import Router from "@/router";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import nprogress from 'nprogress';
+import { useEffect } from 'react';
 import { HelpProvider } from "@/context/HelpProvider";
 import { MultiMamaProvider } from "@/context/MultiMamaContext";
 import { ThemeProvider } from "@/context/ThemeProvider";
 import { Toaster } from "react-hot-toast";
 import CookieConsent from "@/components/CookieConsent";
 
+const queryClient = new QueryClient();
+
 export default function App() {
+  useEffect(() => {
+    nprogress.configure({ showSpinner: false });
+  }, []);
   return (
-    <HelpProvider>
-      <MultiMamaProvider>
-        <ThemeProvider>
-          <Toaster position="top-right" />
-          <Router />
-          <CookieConsent />
-        </ThemeProvider>
-      </MultiMamaProvider>
-    </HelpProvider>
+    <QueryClientProvider client={queryClient}>
+      <HelpProvider>
+        <MultiMamaProvider>
+          <ThemeProvider>
+            <Toaster position="top-right" />
+            <Router />
+            <CookieConsent />
+          </ThemeProvider>
+        </MultiMamaProvider>
+      </HelpProvider>
+    </QueryClientProvider>
   );
 }
 

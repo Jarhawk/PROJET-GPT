db1aebb8972245656fc6efac38152a452eb5e9fb
Jarhawk <dark_territory@msn.com>
2025-07-22 10:47:13 +0200

    Add justificatif column to factures

--- db/Ajout.sql
+++ db/Ajout.sql
@@ -1,3 +1,6 @@
 -- Ajouts complémentaires pour le back-end Supabase
 -- Ajout colonne manquante pour stocker la configuration du tableau de bord
 ALTER TABLE tableaux_de_bord ADD COLUMN IF NOT EXISTS liste_gadgets_json jsonb DEFAULT '[]'::jsonb;
+
+-- Ajout colonne justificatif pour stocker l'URL de la pièce jointe de facture
+ALTER TABLE factures ADD COLUMN IF NOT EXISTS justificatif text;

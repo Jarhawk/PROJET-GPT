cd6671035c3a0eaaaaa3c1aa0967b95620c536fe
Jarhawk <dark_territory@msn.com>
2025-08-14 21:44:34 +0200

    chore(db): cleanup sql scripts and back up master

--- db/v_familles_sous_familles.sql
+++
@@ -1,9 +0,0 @@
--- View to expose familles with their sous-familles using the two-level model
-create or replace view public.v_familles_sous_familles as
-select
-  f.id as famille_id,
-  f.nom as famille_nom,
-  sf.id as sous_famille_id,
-  sf.nom as sous_famille_nom
-from familles f
-left join sous_familles sf on sf.famille_id = f.id;

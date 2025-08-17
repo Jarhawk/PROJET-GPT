d56e6991c5aee7e499e0f5411c4809738afdf7e2
Jarhawk <dark_territory@msn.com>
2025-08-17 14:09:52 +0200

    chore: set query defaults and add product search indexes

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,7 @@ const queryClient = new QueryClient({
       refetchOnWindowFocus: false,
       refetchOnReconnect: false,
       refetchOnMount: false,
+      retry: false,
       staleTime: Infinity,
       gcTime: Infinity,
     },

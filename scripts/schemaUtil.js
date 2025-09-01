b44fb71949f03bc6131359e94e65711f2612acc4
Jarhawk <dark_territory@msn.com>
2025-09-01 10:46:34 +0200

    refactor: remove deprecated supabase env shim

--- scripts/schemaUtil.js
+++ scripts/schemaUtil.js
@@ -6,7 +6,7 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url))
 const schema = JSON.parse(fs.readFileSync(path.join(__dirname, 'schema.json'), 'utf8'))
 
 export function getEntity(name) {
-  return schema.tables[name] || null
+  return schema.tables[name] || schema.views[name] || null
 }
 
 export function getRpc(name) {
